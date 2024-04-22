75fa6d1e1519a495642dd37050329970b2bd21fd
Marcuez Lard <mlard@cs.utexas.edu>
2024-04-22 15:59:45 -0500

    cat is done

---
+++ nikshita.dir/sbin/cat.c
@@ -0,0 +1,12 @@
+#include "libc.h"
+
+int main(int argc, char** argv) {
+    for(int i = 1; i < argc; i++){
+        int fd = open(argv[i], 0x0);
+        if(fd == -1){
+            printf("cat: %s: No such file or directory\n", argv[i]);
+        }else{
+            cp(fd, 1);
+        }
+    }
+}
